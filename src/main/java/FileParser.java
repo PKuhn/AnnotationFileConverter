ac798e28b001ebdf846b7fc5a2ccd9cf840644f7
PKuhn <patrick.kuhn@student.hpi.de>
2015-06-04 15:55:05 +0200

    included Standford Tokenizer

--- src/main/java/FileParser.java
+++ src/main/java/FileParser.java
@@ -1,9 +1,15 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.PTBTokenizer;
 
 public class FileParser {
     public static void main(String[] args) {
@@ -11,8 +17,13 @@ public class FileParser {
         System.out.println(parser.parseConfigurationFile());
         List<AnnotationEntity> entities = parser.readInAnnotationFile("");
 
-        for (AnnotationEntity entity : entities) {
+        /* for (AnnotationEntity entity : entities) {
             System.out.println(entity.getContent());
+        } */
+        try {
+            parser.tokenizeText("");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -25,10 +36,27 @@ public class FileParser {
 
         for (String annotation : annotations) {
             AnnotationEntity entity = new AnnotationEntity(annotation);
-            entities.add(entity); 
+            entities.add(entity);
         }
 
         return entities;
+    }
+    private void tokenizeText(String textName) throws IOException {
+        textName = "antother_text";
+        String fileName = textName += ".txt";
+
+        String text = "";
+        //List<String> lines = Files.readAllLines(Paths.get(fileName), Charset.defaultCharset());
+        //StringReader reader = new StringReader(text);
+        PTBTokenizer<CoreLabel> ptbt = new PTBTokenizer<>(new FileReader(fileName),
+                new CoreLabelTokenFactory(), "");
+        while (ptbt.hasNext()) {
+            CoreLabel label = ptbt.next();
+            System.out.println(label);
+        }
+
+            //List<CoreLabel> rawWords = tokenizerFactory.getTokenizer(new StringReader(sentence)).tokenize();
+        //System.out.println(rawWords.get(0).value());
     }
 
     private List<String> readFileToLines(String fileName) {
